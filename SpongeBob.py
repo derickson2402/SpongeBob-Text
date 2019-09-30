be12d7d61889e6fbeea844aeced8bf6683915b1c
derickson2402 <derickson2402@gmail.com>
2019-09-29 22:28:37 -0400

    Created the Repo

---
+++ SpongeBob.py
@@ -0,0 +1,27 @@
+#Collect the user input and define length
+
+usr_in = input()
+limit = ln(usr_in)
+character = 0
+#Define the even/odd function
+
+
+def SpongeBob()
+    #Using the global variables
+    global usr_in
+    global limit
+    global character
+
+    #Checking for even or odd
+    #I shouldn't have to define global variable again
+    if (character % 2) == 0:
+        print(usr_in.lower()[character])
+        character + 1
+    else:
+        print(usr_in.capitalize()[character])
+        character + 1
+
+#function SpongeBob should be defined by now
+#every other letter should be capitalized
+
+while (limit)
