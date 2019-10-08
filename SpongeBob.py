00d09b02742d933eecaf57695ed176a88ddfb2b4
Daniel Erickson <derickson2402@gmail.com>
2019-10-07 23:15:17 -0400

    Created a new branch with a randomized function

--- SpongeBob.py
+++ SpongeBob.py
@@ -8,20 +8,35 @@ print("\nYou said:", '\n\n', usr_in, end='\n\n')
 #Define the length of the phrase and return the length
 char_lim = len(usr_in)
 print("There are ", char_lim, " characters.", end='\n\n')
-print("Here's your new meme:", end='\n\n')
 
-#Define the function, which will determine if the letter place is even or odd,
+#Determine if the user wants random letters or sequential. Just checks for the
+#letter "r" after making it all lowercase
+function_type = input("Would you like to flip every other letter or go randomly?\n\n")
+print(function_type)
+function_type = function_type.lower()
+print(function_type)
+function_type = ("random" if ("r" in(function_type)) else "sequential")
+print(function_type)
+
+#Define the function, which will determine if the letter index is even or odd,
 # and alter the case accordingly
-def SpongeBob(letter, place):
-    return(letter.lower() if ((place % 2) ==0) else letter.capitalize())
+def SpongeBob_Letter(letter, index):
+    return(letter.lower() if ((index % 2) ==0) else letter.capitalize())
+
+#Define function for randomly altering the lowercase
+def Random_Letter(letter)
+    random_float = random()
+    return(letter.lower() if ((random_float % 2) ==0) else letter.capitalize())
 
 #As long as the character number has not reached, then the function SpongeBob
 # should return each sequential letter in order.
-while (char_lim != char_num):
-    x = SpongeBob(usr_in[char_num], char_num)
+print("Here's your new meme:", end='\n\n')
+while (char_lim != char_num, function_type = sequential):
+    x = SpongeBob_Letter(usr_in[char_num], char_num)
     print(x, end='')
     char_num = (char_num+1)
 print('\n\n', end='')
+
 
 #End the function and say goodbye
 print("Thanks for using my program!\n")
