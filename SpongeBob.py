a31bee9ea1ab24e58610324e6f8d44d5498590ff
Daniel Erickson <derickson2402@gmail.com>
2019-10-12 21:25:29 -0400

    Wrote the randomized function and created a parameter system

--- SpongeBob.py
+++ SpongeBob.py
@@ -12,31 +12,37 @@ print("There are ", char_lim, " characters.", end='\n\n')
 #Determine if the user wants random letters or sequential. Just checks for the
 #letter "r" after making it all lowercase
 function_type = input("Would you like to flip every other letter or go randomly?\n\n")
-print(function_type)
 function_type = function_type.lower()
-print(function_type)
 function_type = ("random" if ("r" in(function_type)) else "sequential")
-print(function_type)
+print("You picked ", function_type, "\n\n")
 
 #Define the function, which will determine if the letter index is even or odd,
 # and alter the case accordingly
-def SpongeBob_Letter(letter, index):
+def SpongeBob_Letter(letter, index): #index is char_num but with makeup on
     return(letter.lower() if ((index % 2) ==0) else letter.capitalize())
 
 #Define function for randomly altering the lowercase
-def Random_Letter(letter)
+def Random_Letter(letter):
     random_float = random()
     return(letter.lower() if ((random_float % 2) ==0) else letter.capitalize())
+    print(random_float)
+print(random_float)
 
 #As long as the character number has not reached, then the function SpongeBob
 # should return each sequential letter in order.
 print("Here's your new meme:", end='\n\n')
-while (char_lim != char_num, function_type = sequential):
-    x = SpongeBob_Letter(usr_in[char_num], char_num)
-    print(x, end='')
-    char_num = (char_num+1)
-print('\n\n', end='')
 
+while (char_lim != char_num, function_type = sequential):
+    x = SpongeBob_Letter(usr_in[char_num], char_num) #char_num becomes index for function
+    print(x, end='')
+    char_num += 1
+
+#Same While loop but for the randomized function
+while (char_lim != char_num, function_type = random):
+    y = Random_Letter(usr_in[char_num])
+    print(y, end='')
+    char_num += 1
 
 #End the function and say goodbye
+print('\n\n', end='')
 print("Thanks for using my program!\n")
