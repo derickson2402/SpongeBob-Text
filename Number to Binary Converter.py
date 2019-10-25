6fd65967c317c688a2450cda533064e3135820a2
Daniel Erickson <derickson2402@gmail.com>
2019-10-24 20:08:00 -0400

    Updated textual interface, but Dec to Bin function gets stuck in loop

--- Number to Binary Converter.py
+++ Number to Binary Converter.py
@@ -1,109 +1,83 @@
 #!/home/derickson/Projects/SpongeBob/Number\ to\ Binary\ Converter.py
-
 print("\n"*20, "This program will take a Binary number and convert it to Decimal,")
 print("or take a Decimal number and convert it to Binary", end="\n\n")
 print("Please note that only integer values are accepted", end= "\n\n")
-Convert_type = input("Would you like to convert 1) Binary to Decimal or 2) Decimal to Binary?\n\n")
-#if ("1" in(Convert_type)):
-#    Convert_type = "BtD"
-#elif ("binary to decimal" in(Convert_type.lower):
-#    Convert_type = "BtD"
-#elif ("2" in(Convert_type)):
-#    Convert_type = "DtB"
-#elif ("decimal to binary" in(Convert_type.lower):
-#    Convert_type = "DtB"
-#else:
-#    print("Bruh try again")
-#    exit()
 
-Convert_type = ("BtD" if ("1" in(Convert_type)) else "DtB")
+def Which_Conversion(conv_type_in):
+    status = "incomplete"
+    while (status == "incomplete"):
+        status = "complete"
+        if ("1" in(conv_type_in.lower())):
+            return("BtD")
+        elif ("binary to decimal" in(conv_type_in.lower())):
+            return("BtD")
+        elif ("2" in(conv_type_in)):
+            return("DtB")
+        elif ("decimal to binary" in(conv_type_in.lower())):
+            return("DtB")
+        else:
+            conv_type_in = input("A whoopsie occured. Try again.\n\n")
+            status = "incomplete"
+    print(end= '\n\n')
 
-#Create global variables to avoid errors
-Convert_number = 0
-Digit_1="0"
-Digit_2="0"
-Digit_3="0"
-Digit_4="0"
-Digit_5="0"
-Digit_6="0"
-Digit_7="0"
-Digit_8="0"
-def Collect_Number():
-    global Convert_number
-    Convert_number = input("\nPlease enter your number below:\n")
-    if ("." in(str(Convert_number))):
-        print("\nStop, you have violated the law!")
-        Convert_number = input("Enter an integer like the directions clearly stated\n")
-Collect_Number()
+def Collect_Number(num_in):
+    status = "incomplete"
+    while (status == "incomplete"):
+        if (("." not in(num_in)) and ("-" not in(num_in)) and (" " not in(num_in))):
+            status = "complete"
+            return(int(num_in))
+        elif ("." in(num_in)):
+            num_in = input("Please enter an integer:\n\n")
+        elif ("-" in(num_in)):
+            num_in = input("Negative values are not accepted, try again:\n\n")
+        elif (" " in(num_in)):
+            num_in = input("Please remove spaces:")
+        else:
+            num_in = input("A whoopsie occured. Try again.\n\n")
 
+Convert_type = Which_Conversion(input("would you like to convert 1) Binary to Decimal or 2) Decimal to Binary?\n\n"))
+print(Convert_type, "\n", type(Convert_type))
+Convert_number = Collect_Number(input("Please enter your number below:\n\n"))
+print(Convert_number, "\n", type(Convert_number))
 
-
-
-
-
-############################################
 ############################################
 #def Bin_to_Dec(Bin_num):
+############################################
 
 def Dec_to_Bin(Dec_num):
-    #Make Dec_num an integer
-    Dec_num = int(Dec_num)
-
-    #Reset the global variables
-    global Digit_1
-    Digit_1 = "0"
-    global Digit_2
-    Digit_2 = "0"
-    global Digit_3
-    Digit_3 = "0"
-    global Digit_4
-    Digit_4 = "0"
-    global Digit_5
-    Digit_5 = "0"
-    global Digit_6
-    Digit_6 = "0"
-    global Digit_7
-    Digit_7 = "0"
-    global Digit_8
-    Digit_8 = "0"
-
-    #Calculate the Binary number
-    while (Dec_num > 0):
+    Running_Binary_Sum = 0
+    active = "true"
+    while (Dec_num > 0, active == "true"):
         if (Dec_num > 255):
             print("Error: number too large, I haven't programmed outside of 8 Bits yet\n")
+            active = "false"
         elif (Dec_num >= 128):
-            Digit_8 = "1"
+            Running_Binary_Sum += 10000000
             Dec_num -= 128
         elif (Dec_num >= 64):
-            Digit_7 = "1"
+            Running_Binary_Sum += 1000000
             Dec_num -= 64
         elif (Dec_num >= 32):
-            Digit_6 = "1"
+            Running_Binary_Sum += 100000
             Dec_num -= 32
         elif (Dec_num >= 16):
-            Digit_5 = "1"
+            Running_Binary_Sum += 10000
             Dec_num -= 16
         elif (Dec_num >= 8):
-            Digit_4 = "1"
+            Running_Binary_Sum += 1000
             Dec_num -= 8
         elif (Dec_num >= 4):
-            Digit_3 = "1"
+            Running_Binary_Sum += 100
             Dec_num -= 4
         elif (Dec_num >= 2):
-            Digit_2 = "1"
+            Running_Binary_Sum += 10
             Dec_num -= 2
         elif (Dec_num >= 1):
-            Digit_1 = "1"
+            Running_Binary_Sum += 1
             Dec_num -= 1
-        else:
-            break
-######################################
-######################################
+            active = "false"
+    return(Running_Binary_Sum)
 
-#if (Convert_type == "DtB" and {[(insert a limit here)]}:
+print("Convert number=", Convert_number)
 Dec_to_Bin(Convert_number)
-print(Digit_8,Digit_7,Digit_6,Digit_5, " ", Digit_4,Digit_3,Digit_2,Digit_1,sep='',end='\n\n')
-#else:
-    #print("A Whoopsie occured!")
-
 print("Thanks for using my program!")
